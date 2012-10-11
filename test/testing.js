fa7295fb370e7e43e9cf306f345d787d59673db7
johnsh <sebastian80_23@hotmail.com>
2012-10-11 17:59:48 -0500

    Commit

--- test/testing.js
+++ test/testing.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 module("Pruebas nodejs");
 test("Test pruebas", function(){
 equal(true, true, "Primero");
-equal(true, false, "segundo");
+equal(true, true, "segundo");
 
 })
 })
